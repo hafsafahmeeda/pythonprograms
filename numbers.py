02919df30f21c1ccaab2f8acfc5fce91b4ce4894
unknown <hafsafahmee@gmail.com>
2020-07-30 20:01:52 +0530

    num

---
+++ numbers.py
@@ -0,0 +1,20 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 56+69
+125
+>>> 75-43
+32
+>>> 43*8
+344
+>>> 58/8
+7.25
+>>> 37%2
+1
+>>> 4**3
+64
+>>> numbers
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    numbers
+NameError: name 'numbers' is not defined
+>>> 
